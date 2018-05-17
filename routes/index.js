05ea3202c9b09bb40c31d09b428f190f8a7dc487
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-05-17 14:06:56 +0300

    enhanced the ui with a logo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LifeAdapter' });
+  res.render('index', { title: req.app.get('appName') });
 });
 
 module.exports = router;
