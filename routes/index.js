71c364000d43af00d8e37658b1fd980ea2dd2a0e
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-05-17 22:48:56 +0300

    added uri decoding for illegal characters in requests

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,24 @@ router.get(/.*/, function (req, res, next) {
   if (req.url.length > 1) {
     // res.redirect(req.app.get('awsLocation') + req.url);
 
-    const key = 'deploy/public' + req.url;
+    const key = req.app.get('awsLocation') + decodeURI(req.url);
+    // console.log('Will request the following key:', key);
 
     params = { Bucket: process.env.S3_BUCKET_NAME, Key: key };
     const object = s3.getObject(params, function (err, data) {
       if (err) {
-        console.log(err)
-        res.writeHead(200, { 'Content-Type': data.ContentType });
-        res.end(null, 'binary');
+        // console.log(err.message);
+        if (data) {
+          // console.log('Gracefully cover up.')
+          res.writeHead(200, { 'Content-Type': data.ContentType });
+          res.end(null, 'binary');
+        } else {
+          // console.log('Blame it on the client.')
+          res.writeHead(418, {});
+          res.end(null, 'binary');
+        }
+        // // console.log('The error was:', err)
+        // console.log('The request was:', req);
       } else {
         res.writeHead(200, { 'Content-Type': data.ContentType });
         res.write(data.Body, 'binary');
