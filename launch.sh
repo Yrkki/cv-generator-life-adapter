989ebaf47fff5bcebcf63d705a2b805e2ea881f3
Jorich <35774231+Yrkki@users.noreply.github.com>
2021-02-03 21:55:37 +0200

    fix(spdy): fix config value type parsing

--- launch.sh
+++ launch.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo Running launch script
-echo ------------------------------------------------------
+echo $'\033[1;33m'Running launch script
+echo ------------------------------------------------------$'\033[1;33m'
 echo
 
-echo Launching server environment...
+echo $'\033[0;33m'Launching server environment...$'\033[0m'
+echo
+pwd=$(pwd)
+pwd
+ls -aF --color=always
+echo
 
 . ./env.sh
 node ./bin/www &
 
-echo Server launched.
+echo
+echo $'\033[1;32m'Server launched.$'\033[0m'
 
 
 echo
