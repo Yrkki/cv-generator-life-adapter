77da8bb4c6dc758c005fc8f5caf9fdabe70ee393
Jorich <35774231+Yrkki@users.noreply.github.com>
2021-05-30 16:33:18 +0300

    ci(amplify): adjust environment

--- public/javascripts/aws.js
+++ public/javascripts/aws.js
@@ -1,7 +1,7 @@
 var AWS = require('aws-sdk');
 var options = {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? process.env.CV_GENERATOR_LIFE_ADAPTER_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? process.env.CV_GENERATOR_LIFE_ADAPTER_AWS_SECRET_ACCESS_KEY
 };
 var endpointS3 = new AWS.S3(options);
 var endpointCloudFront = new AWS.CloudFront({});
