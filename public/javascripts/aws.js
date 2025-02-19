30f90be32644a136707306ffe88a23f40bd20c32
Jorich <35774231+Yrkki@users.noreply.github.com>
2025-02-19 15:30:40 +0200

    refactor(aws-sdk): migrate from v2 to v3

--- public/javascripts/aws.js
+++ public/javascripts/aws.js
@@ -1,9 +1,11 @@
-var AWS = require('aws-sdk');
+var S3 = require('@aws-sdk/client-s3');
+var CloudFront = require('@aws-sdk/client-cloudfront');
+
 var options = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? process.env.CV_GENERATOR_LIFE_ADAPTER_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? process.env.CV_GENERATOR_LIFE_ADAPTER_AWS_SECRET_ACCESS_KEY
 };
-var endpointS3 = new AWS.S3(options);
-var endpointCloudFront = new AWS.CloudFront({});
+var endpointS3 = new S3.S3(options);
+var endpointCloudFront = new CloudFront.CloudFront({});
 
 module.exports = { endpointS3: endpointS3, endpointCloudFront: endpointCloudFront };
